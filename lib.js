08b4b19a4d6628c3eeafde735dacdaad0574aa41
Unknown <JarredNolasco@gmail.com>
2017-03-06 14:56:10 -0600

    Finished Assignment. Code still needs refactoring.

--- lib.js
+++ lib.js
@@ -22,11 +22,37 @@ const listedPrice =
 /**
  * transform carts into an array of { customer, total }
  */
+
 const calculateTotals =
   listings =>
     carts => {
-      // TODO
-    }
+      let bill = []
+
+      const test =
+        (customer, total) => ({
+          customer,
+          total
+        })
+      carts.reduce((result, cart) => {
+        let cartsTotal = 0
+        cart.items.reduce((result,itemName) => {
+
+          listings.reduce((result,listings) => {
+
+            if (itemName === listings.name)
+              cartsTotal = cartsTotal + listings.price
+
+            },'')
+
+
+          },[])
+            bill.push(test(cart.customer,cartsTotal))
+      } ,0)
+
+          return bill
+      }
+
+
 
 module.exports = {
   listing,
--- main.js
+++ main.js
@@ -66,5 +66,5 @@ const saleTotals = calculateTotals(saleListings)
 regularTotals(carts)
   .forEach(cart => console.log(`${cart.customer}, your total is ${cart.total}`))
 
-saleTotals(carts)
-  .forEach(cart => console.log(`${cart.customer}, your total could have been ${cart.total} if you were smart enough to come on a sale day! (you dummy)`))
+  saleTotals(carts)
+   .forEach(cart => console.log(`${cart.customer}, your total could have been ${cart.total} if you were smart enough to come on a sale day! (you dummy)`))
